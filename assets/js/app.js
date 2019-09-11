d6bbaee48b9f0f795c33f788f165327909efa9e9
Cooper Brislain <cbrislain@gmail.com>
2019-09-10 18:13:40 -0700

    added 'more' feature for pagination

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ const api_key = '7rD2m8tjA4uLzOF2ZYyYbx8MGkD0Ymfg';
 
 const google_geocode_api_key = 'AIzaSyAz7eNW1XE8Mb4PQfvetmdAUkOnk9O5qzg';
 let earth; 
+let page = 0;
 
 function initialize() {
     earth = new WE.map('globe');
@@ -11,41 +12,48 @@ function initialize() {
     }).addTo(earth);
 }
 
+function loadStories() {
+    let search_term = $('#searchTerm').val();
+    let query_url  = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search_term}&api-key=${api_key}&page=${page}`
+    $.ajax({
+        url: query_url,
+        method: 'GET'
+    })
+    .then (response => {
+        let results = response.response.docs;
+        results.forEach(result => {
+            let wheres = result.keywords.filter(ob => {
+                return ob.name === 'glocations';
+            });
+            if (wheres.length) {
+                where = wheres[0].value;
+                $.ajax({
+                    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${where}&key=${google_geocode_api_key}`,
+                    method: 'GET'
+                })
+                .then ((response) => {
+                    let location = response.results[0].geometry.location;
+                    let marker = WE.marker([location.lat, location.lng]).addTo(earth);
+                    marker.bindPopup(`<a href="${result.web_url}" target="_blank">${result.headline.main}</a>`, {maxWidth: 150}).openPopup();
+                });
+            }
+        });
+    });
+}
+
 $(document).ready(function() {
     initialize();
 
     $(document).on('click', 'button.btn-search', (e) => {
         e.preventDefault();
-        let search_term = $('#searchTerm').val();
-        let num_records = $('#numRecords').val();
-        let year_start = $('#startYear').val()? `${$('#startYear').val()}0101` : '';
-        let year_end = $('#endYear').val()? `${$('#endYear').val()}1231` : '' ;
-        let query_url  = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search_term}&api-key=${api_key}`
-        $.ajax({
-            url: query_url,
-            method: 'GET'
-        })
-        .then (response => {
-            let results = response.response.docs;
-            results.forEach(result => {
-                let wheres = result.keywords.filter(ob => {
-                    return ob.name === 'glocations';
-                });
-                if (wheres.length) {
-                    where = wheres[0].value;
-                    $.ajax({
-                        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${where}&key=${google_geocode_api_key}`,
-                        method: 'GET'
-                    })
-                    .then ((response) => {
-                        let location = response.results[0].geometry.location;
-                        console.log(location);
-                        let marker = WE.marker([location.lat, location.lng]).addTo(earth);
-                        marker.bindPopup(`<a href="${result.web_url}" target="_blank">${result.headline.main}</a>`, {maxWidth: 150}).openPopup();
-                    });
-                }
-            });
-        });
+        page = 1;
+        loadStories();
+    });
+
+    $(document).on('click', 'button.btn-more', (e) => {
+        e.preventDefault();
+        page++;
+        loadStories();
     });
 
     $(document).on('click', 'button.btn-clear', (e) => {
