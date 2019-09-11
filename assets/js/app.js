5d4055bd475cb07a618232e291310b9f2dd45574
Cooper Brislain <cbrislain@gmail.com>
2019-09-10 18:19:31 -0700

    halved speed

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,7 +15,7 @@ function initialize() {
         var c = earth.getPosition();
         var elapsed = before? now - before: 0;
         before = now;
-        earth.setCenter([c[0], c[1] + 0.1*(elapsed/30)]);
+        earth.setCenter([c[0], c[1] + 0.05*(elapsed/30)]);
         requestAnimationFrame(animate);
     });
 }
